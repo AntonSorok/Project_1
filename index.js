6cb4a146ae3cd276d296b07c6ab38f34aa49a0a0
AntonSorok <anton.1998@inbox.ru>
2026-08-13 00:09:27 +0900

    WIP: nav absolute positioning, overlay z-index issue.

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-console.log('hello world!')
+// const backgroundMenu = document.querySelector('.backgroundMenu')
+// const menuOpen = document.querySelector('.menu-close')
+// const menuClose = document.querySelector('.menu-open')
+//
+// menuOpen.addEventListener('click', (e) => {
+//     e.preventDefault();
+//
+// backgroundMenu.classList.toggle('active')
+// })
+//
+// menuClose.addEventListener('click', (e) => {
+//     e.preventDefault();
+//     backgroundMenu.classList.toggle('active')
+// })
+
+
+const burger = document.querySelector('.nav__burger')
+
+burger.addEventListener('click', (e) => {
+    e.preventDefault()
+    const isOpen = document.body.classList.toggle('menu-open')
+    burger.setAttribute('aria-expanded', isOpen)
+})
