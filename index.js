df3ef2e33e5938bdaba707ea5bedb98d63c69677
AntonSorok <anton.1998@inbox.ru>
2026-08-13 23:05:06 +0900

    finish nav panel. Fix styles. Add anchors.

--- index.js
+++ index.js
@@ -12,12 +12,25 @@
 //     e.preventDefault();
 //     backgroundMenu.classList.toggle('active')
 // })
-
+// const headerAnchor = document.getElementById("header");
+// const introAnchor = document.getElementById("intro");
+// const galleryAnchor = document.getElementById("gallery");
+// const testimonialsAnchor = document.getElementById("testimonials");
+// const contactsAnchor = document.getElementById("contacts");
 
 const burger = document.querySelector('.nav__burger')
+const menuLinks = document.querySelectorAll('.overlay__list a')
 
 burger.addEventListener('click', (e) => {
     e.preventDefault()
     const isOpen = document.body.classList.toggle('menu-open')
     burger.setAttribute('aria-expanded', isOpen)
 })
+
+menuLinks.forEach(link => {
+    link.addEventListener('click', (e) => {
+        document.body.classList.remove('menu-open')
+        burger.setAttribute('aria-expanded', 'false')
+    })
+})
+
